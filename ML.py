3f517de26edf832b5043f885df8c3858d6fa8239
hiwothadush <136127818+hiwothadush@users.noreply.github.com>
2024-02-19 21:58:02 -0800

    first commit

---
+++ ML.py
@@ -0,0 +1,13 @@
+"""
+this is all about 
+basic machine learning
+and basic data science
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# import sklearn
+x = np.array([1, 2, 3, 4,5])
+y = np.array([3, 6, 8, 9, 3])
+plt.scatter(x, y)
+plt.show()
